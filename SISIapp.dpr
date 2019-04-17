a07a0a8a8069811dfcd09f4daf36963fe2f1b0cb
nathalyas <nathalyas@outlook.com>
2019-04-17 02:54:13 -0300

    Delete unused files

--- SISIapp.dpr
+++ SISIapp.dpr
@@ -1,13 +1,14 @@
 program SISIapp;
 
+
+
 uses
   System.StartUpCopy,
   FMX.Forms,
-  u_piloto in 'u_piloto.pas' {frm_piloto},
   login in 'login.pas' {frm_login},
-  mapa in 'mapa.pas' {Form4},
   listaOcorrencias in 'listaOcorrencias.pas' {Form3},
-  cadastro in 'cadastro.pas' {Form2};
+  cadastro in 'cadastro.pas' {Form2},
+  home in 'home.pas';
 
 {$R *.res}
 
