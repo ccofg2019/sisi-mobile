80cf7dbb17b268582f684109a816eb7453a88026
nathalyas <nathalyas@outlook.com>
2019-04-21 16:43:30 -0300

    Tela de listagem de ocorrências

--- listaOcorrencias.pas
+++ listaOcorrencias.pas
@@ -13,14 +13,31 @@ type
     Rectangle10: TRectangle;
     menu: TSpeedButton;
     title: TText;
-    VertScrollBox1: TVertScrollBox;
-    Layout1: TLayout;
+    list: TVertScrollBox;
+    listItem: TLayout;
     Rectangle1: TRectangle;
     Text1: TText;
     Text2: TText;
     Text3: TText;
     RoundRect1: TRoundRect;
+    infoLeft: TLayout;
+    infoRight: TLayout;
+    Layout1: TLayout;
+    Rectangle2: TRectangle;
+    Layout2: TLayout;
+    Rectangle4: TRoundRect;
     Text4: TText;
+    Layout3: TLayout;
+    Text5: TText;
+    Text6: TText;
+    Layout4: TLayout;
+    Rectangle3: TRectangle;
+    Layout5: TLayout;
+    RoundRect3: TRoundRect;
+    Text7: TText;
+    Layout6: TLayout;
+    Text8: TText;
+    Text9: TText;
   private
     { Private declarations }
   public
